a0291010c31afc62961f69ca771b708c82c45f00
Elisha Bello <elishabello2014@gmail.com>
2022-03-22 23:29:14 +0100

    Production bug fix

--- server/model/index.js
+++ server/model/index.js
@@ -2,18 +2,30 @@ import Sequelize from "sequelize";
 import UserModel from "./user.model.js";
 import {default as dbConfig} from "../../config/db_config.js";
 
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-    host: dbConfig.HOST,
-    dialect: dbConfig.dialect,
-    logging: dbConfig.LOGGING,
-    pool: {
-        max: dbConfig.pool.max,
-        min: dbConfig.pool.min,
-        acquire: dbConfig.pool.acquire,
-        idle: dbConfig.pool.idle
+// const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
+//     host: dbConfig.HOST,
+//     dialect: dbConfig.dialect,
+//     logging: dbConfig.LOGGING,
+//     pool: {
+//         max: dbConfig.pool.max,
+//         min: dbConfig.pool.min,
+//         acquire: dbConfig.pool.acquire,
+//         idle: dbConfig.pool.idle
+//     }
+// });
+const sequelize = new Sequelize(
+    process.env.DB_URL, // your database url
+    {
+        dialect: 'postgres',
+        protocol: 'postgres',
+        dialectOptions: {
+            ssl: {
+                require: true,
+                rejectUnauthorized: false
+            }
+        }
     }
-});
-
+);
 const db = {};
 
 db.User = UserModel(sequelize, Sequelize.DataTypes);
